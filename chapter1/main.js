95e734b3e49285f7db00e1ed27c3866a161a2c40
JongWoo Park <jjongwoo.dev@gmail.com>
2021-07-11 18:46:29 +0900

    chapter1: 중간 점검
    - statement 함수 안에 모든 로직 집어넣음.

--- chapter1/main.js
+++ chapter1/main.js
@@ -1,65 +1,4 @@
-function totalAmount(invoice) {
-  let result = 0;
-  for(let perf of invoice.performances){
-    result += amountFor(perf);
-  }
-  return result;
-}
-
-function usd(aNumber){
-  return new Intl.NumberFormat("en-US", {
-    style: "currency", currency: "USD", minimumFactionDigits: 2
-  }).format(aNumber/100);
-}
-
-function totalVolumeCredits(invoice){
-  let result = 0;
-  for(let perf of invoice.performances){
-    result += volumeCreditsFor(perf);
-  }
-  return result;
-}
-
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  // 포인트를 적립한다.
-  result += Math.max(aPerformance.audience - 30, 0);
-
-  // 희극 관객 5명마다 추가 포인트를 제공한다.
-  if ("comedy" === playFor(aPerformance).type){
-    result += Math.floor(aPerformance.audience / 5);
-  }
-  return result;
-}
-
-
-function playFor(aPerformance){
-  return plays[aPerformance.playID];
-}
-
-function amountFor(aPerformance){
-  let result = 0;
-  switch(playFor(aPerformance).type){
-    case "tragedy":
-      result = 40000;
-      if(aPerformance.audience > 30){
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-      case "comedy":
-        result = 30000;
-        if(aPerformance.audience > 20){
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown genre: ${playFor(aPerformance).type}`);
-  }
-  return result;
-}
-
-function statement(invoice, plays){
+function statement(invoice, plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances){
@@ -70,6 +9,68 @@ function statement(invoice, plays){
   result += `총액 ${usd(totalAmount(invoice))}\n`;
   result += `적립 포인트: ${totalVolumeCredits(invoice)} 점`;
   return result;
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances){
+      result += amountFor(perf);
+    }
+    return result;
+  }
+
+  function usd(aNumber){
+    return new Intl.NumberFormat("en-US", {
+      style: "currency", currency: "USD", minimumFactionDigits: 2
+    }).format(aNumber/100);
+  }
+
+  function totalVolumeCredits(){
+    let result = 0;
+    for(let perf of invoice.performances){
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    // 포인트를 적립한다.
+    result += Math.max(aPerformance.audience - 30, 0);
+
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ("comedy" === playFor(aPerformance).type){
+      result += Math.floor(aPerformance.audience / 5);
+    }
+    return result;
+  }
+
+
+  function playFor(aPerformance){
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance){
+    let result = 0;
+    switch(playFor(aPerformance).type){
+      case "tragedy":
+        result = 40000;
+        if(aPerformance.audience > 30){
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+        case "comedy":
+          result = 30000;
+          if(aPerformance.audience > 20){
+            result += 10000 + 500 * (aPerformance.audience - 20);
+          }
+          result += 300 * aPerformance.audience;
+          break;
+        default:
+          throw new Error(`unknown genre: ${playFor(aPerformance).type}`);
+    }
+    return result;
+  }
+
 }
 
 const invoices = require('./invoices.json');
