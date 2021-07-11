c21bf8a9257b0f16a1079482704ac33a00c8061b
JongWoo Park <jjongwoo.dev@gmail.com>
2021-07-11 17:40:47 +0900

    chapter1: volumeCredits - 인라인 변수

--- chapter1/main.js
+++ chapter1/main.js
@@ -61,9 +61,8 @@ function statement(invoice, plays){
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = totalVolumeCredits(invoice);
   result += `총액 ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits} 점`;
+  result += `적립 포인트: ${totalVolumeCredits(invoice)} 점`;
   return result;
 }
 
