79aee8b20ea3071db75c3fdec2de2fb3336c66ea
JongWoo Park <jjongwoo.dev@gmail.com>
2021-07-11 16:02:01 +0900

    chapter1: play 변수 제거하기

--- chapter1/main.js
+++ chapter1/main.js
@@ -2,7 +2,7 @@ function playFor(aPerformance){
   return plays[aPerformance.playID];
 }
 
-function amountFor(aPerformance, play){
+function amountFor(aPerformance){
   let result = 0;
   switch(playFor(aPerformance).type){
     case "tragedy":
@@ -33,7 +33,7 @@ function statement(invoice, plays){
   }).format;
 
   for (let perf of invoice.performances){
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
