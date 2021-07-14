d12f9f3cd8b93a1d4cbd84673a8e98ab4f852b9b
JongWoo Park <jjongwoo.dev@gmail.com>
2021-07-15 01:01:59 +0900

    chapter1: 1.6 공견 정보를 중간 데이터로 옮김

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -1,19 +1,20 @@
 function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
   return renderPlainText(statementData, invoice, plays);
 }
 
 function renderPlainText(data, invoice, plays){
   let result = `청구 내역 (고객명: ${data.customer})\n`;
 
-  for (let perf of invoice.performances){
+  for (let perf of data.performances){
     // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} 석)\n`;
   }
 
-  result += `총액 ${usd(totalAmount(invoice))}\n`;
-  result += `적립 포인트: ${totalVolumeCredits(invoice)} 점`;
+  result += `총액 ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()} 점`;
   return result;
 
   function totalAmount() {
