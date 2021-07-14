5546e29c82b05e4fe17a3741357156ef7ad35f9f
JongWoo Park <jjongwoo.dev@gmail.com>
2021-07-15 01:19:20 +0900

    chapter1: 1.6 amountFor 함수 옮기기

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -9,6 +9,7 @@ function enrichPerformance(aPerformance){
   // 가변 데이터는 금방 상하기 때문에 데이터는 최대한 불변처럼 취급한다.
   const result = Object.assign({}, aPerformance);
   result.play = playFor(result);
+  result.amount = amountFor(result);
   return result;
 }
 
@@ -16,12 +17,34 @@ function playFor(aPerformance){
   return plays[aPerformance.playID];
 }
 
+function amountFor(aPerformance){
+  let result = 0;
+  switch(aPerformance.play.type){
+    case "tragedy":
+      result = 40000;
+      if(aPerformance.audience > 30){
+        result += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+      case "comedy":
+        result = 30000;
+        if(aPerformance.audience > 20){
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown genre: ${aPerformance.play.type}`);
+  }
+  return result;
+}
+
 function renderPlainText(data, plays){
   let result = `청구 내역 (고객명: ${data.customer})\n`;
 
   for (let perf of data.performances){
     // 청구 내역을 출력한다.
-    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} 석)\n`;
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} 석)\n`;
   }
 
   result += `총액 ${usd(totalAmount())}\n`;
@@ -31,7 +54,7 @@ function renderPlainText(data, plays){
   function totalAmount() {
     let result = 0;
     for (let perf of data.performances){
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result;
   }
@@ -58,30 +81,6 @@ function renderPlainText(data, plays){
     // 희극 관객 5명마다 추가 포인트를 제공한다.
     if ("comedy" === aPerformance.play.type){
       result += Math.floor(aPerformance.audience / 5);
-    }
-    return result;
-  }
-
-
-
-  function amountFor(aPerformance){
-    let result = 0;
-    switch(aPerformance.play.type){
-      case "tragedy":
-        result = 40000;
-        if(aPerformance.audience > 30){
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-        case "comedy":
-          result = 30000;
-          if(aPerformance.audience > 20){
-            result += 10000 + 500 * (aPerformance.audience - 20);
-          }
-          result += 300 * aPerformance.audience;
-          break;
-        default:
-          throw new Error(`unknown genre: ${aPerformance.play.type}`);
     }
     return result;
   }
