a029045538e2d295c9fea77f3d24c01645446844
JongWoo Park <jjongwoo.dev@gmail.com>
2021-07-15 01:06:41 +0900

    chapter1: 1.6 invoice를 중간 데이터로 옮김

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -2,10 +2,10 @@ function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances;
-  return renderPlainText(statementData, invoice, plays);
+  return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(data, invoice, plays){
+function renderPlainText(data, plays){
   let result = `청구 내역 (고객명: ${data.customer})\n`;
 
   for (let perf of data.performances){
@@ -19,7 +19,7 @@ function renderPlainText(data, invoice, plays){
 
   function totalAmount() {
     let result = 0;
-    for (let perf of invoice.performances){
+    for (let perf of data.performances){
       result += amountFor(perf);
     }
     return result;
@@ -33,7 +33,7 @@ function renderPlainText(data, invoice, plays){
 
   function totalVolumeCredits(){
     let result = 0;
-    for(let perf of invoice.performances){
+    for(let perf of data.performances){
       result += volumeCreditsFor(perf);
     }
     return result;
