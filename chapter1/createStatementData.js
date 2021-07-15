a3fae6147ae1bc4b4166d9e66c15d16f6b9e56fa
JongWoo Park <jjongwoo.dev@gmail.com>
2021-07-16 00:18:05 +0900

    chapter1: 1.8 함수 옮기기

--- chapter1/createStatementData.js
+++ chapter1/createStatementData.js
@@ -12,7 +12,7 @@ function createStatementData(invoice, plays) {
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
-        result.amount = amountFor(result);
+        result.amount = calculator.amount;
         result.volumeCredits = volumeCreditsFor(result);
         return result;
       }
@@ -22,25 +22,7 @@ function createStatementData(invoice, plays) {
       }
     
       function amountFor(aPerformance){
-        let result = 0;
-        switch(aPerformance.play.type){
-          case "tragedy":
-            result = 40000;
-            if(aPerformance.audience > 30){
-              result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-            case "comedy":
-              result = 30000;
-              if(aPerformance.audience > 20){
-                result += 10000 + 500 * (aPerformance.audience - 20);
-              }
-              result += 300 * aPerformance.audience;
-              break;
-            default:
-              throw new Error(`unknown genre: ${aPerformance.play.type}`);
-        }
-        return result;
+        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
       }
     
       function volumeCreditsFor(aPerformance) {
@@ -66,6 +48,27 @@ class PerformanceCalculator{
   constructor(aPerformance, play){
     this.performance = aPerformance;
     this.play = play;
+  }
+  get amount(){
+    let result = 0;
+    switch(this.play.type){
+      case "tragedy":
+        result = 40000;
+        if(this.performance.audience > 30){
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+        case "comedy":
+          result = 30000;
+          if(this.performance.audience > 20){
+            result += 10000 + 500 * (this.performance.audience - 20);
+          }
+          result += 300 * this.performance.audience;
+          break;
+        default:
+          throw new Error(`unknown genre: ${this.performance.play.type}`);
+    }
+    return result;
   }
 }
 
