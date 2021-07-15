99830a809d3eee7fc77a2a0fc3c50c47bbff1d42
JongWoo Park <jjongwoo.dev@gmail.com>
2021-07-15 23:14:27 +0900

    chapter1: 1.6 html 버전 추가 및 테스트 추가

--- chapter1/test.js
+++ chapter1/test.js
@@ -1,4 +1,4 @@
-const {statement} = require('./statement');
+const {statement, htmlStatement} = require('./statement');
 const mock = require('./mock');
 const invoices = mock.invoices;
 const plays = mock.plays;
@@ -11,12 +11,27 @@ const expectedPlainText = `청구 내역 (고객명: BigCo)
 총액 $1,730.00
 적립 포인트: 47 점`;
 
+const expectedHtml = `<h1>청구 내역 (고객명: BigCo)</h1>
+<table>
+<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>
+<tr><td>Hamlet</td><td>(55석)</td><td>$650.00</td></tr>
+<tr><td>As You Like It</td><td>(35석)</td><td>$580.00</td></tr>
+<tr><td>Othello</td><td>(40석)</td><td>$500.00</td></tr>
+</table>
+<p>총액: <em>$1,730.00</em></p>
+<p>적립 포인트: <em>47</em>점</p>
+`;
+
 
 describe('Chapter1', () => {
   describe('plainText', () => {
     it('should return formatted string', () => { 
-    
       assert.equal(statement(invoices[0], plays), expectedPlainText); 
+    });
+  });
+  describe('htmlVersion', () => {
+    it('should return in html form', () => {
+      assert.equal(htmlStatement(invoices[0], plays), expectedHtml); 
     });
   });
 });
