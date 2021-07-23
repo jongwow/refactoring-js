b6e7e71c935b199c4ceba58f62455b31e195c84f
JongWoo Park <jjongwoo.dev@gmail.com>
2021-07-23 09:43:18 +0900

    test(chapter4): 4.6 경계 조건 검사하기

--- chapter4/main.js
+++ chapter4/main.js
@@ -142,4 +142,50 @@ describe('province', function () {
     expect(asia.shortfall).equal(-6);
     expect(asia.profit).equal(292);
   })
+  it('zero demnad', function () {
+    asia.demand = 0;
+    expect(asia.shortfall).equal(-25);
+    expect(asia.profit).equal(0);
+  });
+  it('negative demand', function () {
+    asia.demand = -1;
+    expect(asia.shortfall).equal(-26);
+    expect(asia.profit).equal(-10);
+  });
+  it('empty string demand', function () {
+    asia.demand = '';
+    expect(asia.shortfall).NaN;
+    expect(asia.profit).NaN;
+  });
+});
+describe('no producers', function () {
+  let noProducers;
+  beforeEach(function () {
+    const data = {
+      name: "No producers",
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+    noProducers = new Province(data);
+  });
+  it('shortfall', function () {
+    expect(noProducers.shortfall).equal(30);
+  })
+  it('profit', function () {
+    expect(noProducers.profit).equal(0);
+  })
 })
+
+describe('string for producers', function () { // 생산자 수 필드에 배열 대신 문자열 대입
+  it('', function () {
+    const data = {
+      name: 'String producers',
+      producers: '',
+      demand: 30,
+      price: 20,
+    }
+    const prov = new Province(data);
+    expect(prov.shortfall).equal(0);
+  });
+});
